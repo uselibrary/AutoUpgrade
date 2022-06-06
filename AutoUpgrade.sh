1948a78205f6cc44c3969b3ea15fe69aa81ed027
uselibrary <emkqson@outlook.com>
2022-06-06 10:06:10 +0800

    logic update

--- AutoUpgrade.sh
+++ AutoUpgrade.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Auto Upgrade for Debian, Version 0.1
+# Auto Upgrade for Debian, Version 0.2
 # June 2, 2022. via htttps://PA.CI
 
 # Token and ChatID of telegram bot
@@ -8,13 +8,13 @@ Token=
 ChatID=
 
 # grep upgradable info, and count of upgradable
+apt update
 UpgradeInfo=$(apt list --upgradable)
 UpgradableCount=$(($(echo -n "$UpgradeInfo" | grep -c '^')-1))
 
 # upgrade
 if [ $UpgradableCount -ne 0 ]
 then
-    apt update
     apt upgrade -y
     echo "Sleep 5 sec for CPU load."
     sleep 5
@@ -24,4 +24,14 @@ then
     echo "$time" >> /usr/local/AutoUpgrade/upgrade.log
     echo "$UpgradeInfo" >> /usr/local/AutoUpgrade/upgrade.log
     echo ""
+fi
+
+# upgrade and log
+if [ $UpgradableCount -eq 0 ]
+then
+    echo "$time" >> /usr/local/AutoUpgrade/upgrade.log
+    echo "No upgradable" >> /usr/local/AutoUpgrade/upgrade.log
+    echo ""
+else
+
 fi
