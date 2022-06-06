5f997d7c462e248fc2a7b560b690cb284027afbf
uselibrary <emkqson@outlook.com>
2022-06-06 10:08:18 +0800

    update

--- AutoUpgrade.sh
+++ AutoUpgrade.sh
@@ -13,8 +13,26 @@ UpgradeInfo=$(apt list --upgradable)
 UpgradableCount=$(($(echo -n "$UpgradeInfo" | grep -c '^')-1))
 
 # upgrade
-if [ $UpgradableCount -ne 0 ]
+# if [ $UpgradableCount -ne 0 ]
+# then
+#     apt upgrade -y
+#     echo "Sleep 5 sec for CPU load."
+#     sleep 5
+#     apt autoremove -y
+#     time=$(date +%F\ %T)
+#     curl -s -o /dev/null "https://api.telegram.org/bot$Token/sendMessage?chat_id=$ChatID&text=$UpgradableCount upgraded at $time"
+#     echo "$time" >> /usr/local/AutoUpgrade/upgrade.log
+#     echo "$UpgradeInfo" >> /usr/local/AutoUpgrade/upgrade.log
+#     echo ""
+# fi
+
+# upgrade and log
+if [ $UpgradableCount -eq 0 ]
 then
+    echo "$time" >> /usr/local/AutoUpgrade/upgrade.log
+    echo "No upgradable" >> /usr/local/AutoUpgrade/upgrade.log
+    echo ""
+else
     apt upgrade -y
     echo "Sleep 5 sec for CPU load."
     sleep 5
@@ -24,14 +42,4 @@ then
     echo "$time" >> /usr/local/AutoUpgrade/upgrade.log
     echo "$UpgradeInfo" >> /usr/local/AutoUpgrade/upgrade.log
     echo ""
-fi
-
-# upgrade and log
-if [ $UpgradableCount -eq 0 ]
-then
-    echo "$time" >> /usr/local/AutoUpgrade/upgrade.log
-    echo "No upgradable" >> /usr/local/AutoUpgrade/upgrade.log
-    echo ""
-else
-
 fi
